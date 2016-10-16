fbc952c6a2c7fe5efeef01618223a25a45859f61
Larry Lade <lplade@users.noreply.github.com>
2016-10-16 03:08:06 -0500

    Pull in new method code from Clara's repo

--- src/name/lade/TicketManager.java
+++ src/name/lade/TicketManager.java
@@ -10,7 +10,7 @@ public class TicketManager {
 
         Scanner scan = new Scanner(System.in);
 
-        while(true){
+        while (true) {
 
             System.out.println("1. Enter Ticket\n2. Delete Ticket\n3. Display All Tickets\n4. Quit");
             int task = Integer.parseInt(scan.nextLine());
@@ -23,12 +23,11 @@ public class TicketManager {
                 //delete a ticket
                 deleteTicket(ticketQueue);
 
-            } else if ( task == 4 ) {
+            } else if (task == 4) {
                 //Quit. Future prototype may want to save all tickets to a file
                 System.out.println("Quitting program");
                 break;
-            }
-            else {
+            } else {
                 //this will happen for 3 or any other selection that is a valid int
                 //TODO Program crashes if you enter anything else - please fix
                 //Default will be print all tickets
@@ -40,7 +39,6 @@ public class TicketManager {
 
     }
 
-    //TODO complete this method
     protected static void deleteTicket(LinkedList<Ticket> ticketQueue) {
         printAllTickets(ticketQueue);   //display list for user
 
@@ -86,7 +84,7 @@ public class TicketManager {
         Date dateReported = new Date(); //Default constructor creates date with current date/time
         int priority;
 
-        while (moreProblems){
+        while (moreProblems) {
             System.out.println("Enter problem");
             description = sc.nextLine();
             System.out.println("Who reported this issue?");
@@ -95,7 +93,8 @@ public class TicketManager {
             priority = Integer.parseInt(sc.nextLine());
 
             Ticket t = new Ticket(description, priority, reporter, dateReported);
-            ticketQueue.add(t);
+            //ticketQueue.add(t);
+            addTicketInPriorityOrder(ticketQueue, t);
 
             //To test, let's print out all of the currently stored tickets
             printAllTickets(ticketQueue);
@@ -106,12 +105,41 @@ public class TicketManager {
                 moreProblems = false;
             }
         }
-
     }
+
+
+    protected static void addTicketInPriorityOrder(LinkedList<Ticket> tickets, Ticket newTicket) {
+
+        //Logic: assume the list is either empty or sorted
+
+        if (tickets.size() == 0) {//Special case - if list is empty, add ticket and return
+            tickets.add(newTicket);
+            return;
+        }
+
+        //Tickets with the HIGHEST priority number go at the front of the list. (e.g. 5=server on fire)
+        //Tickets with the LOWEST value of their priority number (so the lowest priority) go at the end
+
+        int newTicketPriority = newTicket.getPriority();
+
+        for (int x = 0; x < tickets.size(); x++) {    //use a regular for loop so we know which element we are looking at
+
+            //if newTicket is higher or equal priority than the this element, add it in front of this one, and return
+            if (newTicketPriority >= tickets.get(x).getPriority()) {
+                tickets.add(x, newTicket);
+                return;
+            }
+        }
+
+        //Will only get here if the ticket is not added in the loop
+        //If that happens, it must be lower priority than all other tickets. So, add to the end.
+        tickets.addLast(newTicket);
+    }
+
     protected static void printAllTickets(LinkedList<Ticket> tickets) {
         System.out.println(" ------- All open tickets ----------");
 
-        for (Ticket t : tickets ) {
+        for (Ticket t : tickets) {
             System.out.println(t); //Write a toString method in Ticket class
             //println will try to call toString on its argument
         }
